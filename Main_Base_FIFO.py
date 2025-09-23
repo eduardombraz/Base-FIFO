48f8ed6936ba49c13337d354ad8d154eebb90b85
eduardombraz <eduardo.louzada@shopee.com>
2025-09-23 14:51:36 -0300

    Update Main_Base_FIFO.py

--- Main_Base_FIFO.py
+++ Main_Base_FIFO.py
@@ -143,7 +143,9 @@ async def main():
             
             # DOWNLOAD
             async with page.expect_download() as download_info:
-                await page.get_by_role('button', name='Baixar').first.click()
+                await page.goto("https://spx.shopee.com.br/#/orderTracking")
+                await page.locator('xpath=/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[8]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/table[1]/tbody[2]/tr[1]/td[7]/div[1]/div[1]/button[1]/span[1]/span[1]').click()
+                #await page.get_by_role('button', name='Baixar').first.click()
             
             download = await download_info.value
             download_path = os.path.join(DOWNLOAD_DIR, download.suggested_filename)
